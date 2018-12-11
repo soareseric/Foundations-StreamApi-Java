c35bc2d2ffb612a6d20925a13e0ee7957e17a9e7
soareseric <contato.eric.sm@gmail.com>
2018-12-11 10:56:26 -0200

    Projeto criado

---
+++ src/br/com/ericsoares/stream/api/main/Main.java
@@ -0,0 +1,9 @@
+package br.com.ericsoares.stream.api.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/br/com/ericsoares/stream/api/models/Empregado.java
@@ -0,0 +1,42 @@
+package br.com.ericsoares.stream.api.models;
+
+public class Empregado {
+
+	private Long id;
+	private String name;
+	private double salario;
+	private String departamento;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+}
