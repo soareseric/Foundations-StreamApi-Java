6e10cd4a7b0a9eff6e2cb882637008f4845bab95
soareseric <contato.eric.sm@gmail.com>
2018-12-11 12:47:22 -0200

    Interfaces Lambda: Predicate e Supplier

--- src/br/com/ericsoares/stream/api/main/Main.java
+++ src/br/com/ericsoares/stream/api/main/Main.java
@@ -2,9 +2,12 @@ package br.com.ericsoares.stream.api.main;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 import br.com.ericsoares.stream.api.impl.Mensageiro;
 import br.com.ericsoares.stream.api.models.Empregado;
@@ -68,5 +71,25 @@ public class Main {
 		BinaryOperator<Empregado> binaryOperator = (emp1, emp2) -> new Empregado(2, emp1.getNome() + emp2.getNome(), emp1.getSalario() + emp2.getSalario(), emp1.getDepartamento() + emp2.getDepartamento());
 		Empregado novoEmpregado = binaryOperator.apply(new Empregado(10, "Eric", 10000, "Analista de "), new Empregado(11, " Soares", 20000, "Sistemas"));
 		System.out.printf("O novo empregadado se chama %s e seu salario é R$ %.2f e ele é %s", novoEmpregado.getNome(), novoEmpregado.getSalario(), novoEmpregado.getDepartamento());
+		System.out.println();
+		System.out.println();
+		
+		
+		// Predicate
+		// Apenas retorna um Boolean
+		System.out.println("Execução do Predicate: ");
+		Predicate<Empregado> predicate = (emp) -> emp.getNome().endsWith("Soares");
+		Boolean terminaComWeb = predicate.test(new Empregado(1, "Eric Soares", 30000, ""));
+		System.out.println("O nome do empregado termina com Soares? " + terminaComWeb);
+		System.out.println();
+		
+		// Supplier
+		// 
+		System.out.println("Execução do Supplier: ");
+		Supplier<Empregado> supplier = () -> new Empregado(new Random().nextInt(), "", 0, "");
+		Empregado emp1 = supplier.get();
+		System.out.println(emp1.getId());
+		Empregado emp2 = supplier.get();
+		System.out.println(emp2.getId());
 	}
 }
