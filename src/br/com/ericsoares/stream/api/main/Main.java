3a395e9322cf75ac91a4325c7021a4790ff42536
soareseric <contato.eric.sm@gmail.com>
2018-12-11 15:02:46 -0200

    Stream API: Reference method, método Summarizing

--- src/br/com/ericsoares/stream/api/main/Main.java
+++ src/br/com/ericsoares/stream/api/main/Main.java
@@ -1,6 +1,7 @@
 package br.com.ericsoares.stream.api.main;
 
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.OptionalDouble;
 import java.util.stream.Collectors;
@@ -18,25 +19,30 @@ public class Main {
 		empregados.add(new Empregado(3, "Tadeu", 6000, "Analista"));
 
 		System.out.println("** FUNCIONÁROS QUE COMEÇAM COM J **");
-		//STREAM NÃO PODEM SER REAPROVEITADOS
-		//STREAM PODEM SOFRER AÇÕES DE TERMINAÇÃO
-		
+		// STREAM NÃO PODEM SER REAPROVEITADOS
+		// STREAM PODEM SOFRER AÇÕES DE TERMINAÇÃO
+
 		Stream<Empregado> stream = empregados.stream().filter(emp -> {
 			System.out.println("invocando o método filter");
 			return emp.getNome().startsWith("J");
 		});
-		System.out.println("A Stream API é lazy loading! Comprovado com esse sysout. Que foi executado primeiro que o sysout do filter");
+		System.out.println(
+				"A Stream API é lazy loading! Comprovado com esse sysout. Que foi executado primeiro que o sysout do filter");
 		List<Empregado> empregadosComJ = stream.collect(Collectors.toList());
-		
+
 		empregadosComJ.stream().forEach((emp) -> System.out.println(emp.getNome()));
 		OptionalDouble menorSalario = empregadosComJ.stream().mapToDouble((emp) -> emp.getSalario()).min();
 		if (menorSalario.isPresent()) {
 			System.out.println("O menor salario é R$ " + menorSalario.getAsDouble());
 		}
 		
-		
-		
-		
-		
+		DoubleSummaryStatistics sumario = empregados.stream().collect(Collectors.summarizingDouble(Empregado::getSalario)); // Reference method, pode ser usado no lugar de expressoes lambdas
+		System.out.println("Estatisticas do salário: ");
+		System.out.println("Maior salário: " + sumario.getMax());
+		System.out.println("Menor salário: " + sumario.getMin());
+		System.out.printf("Média salarial: %.1f", sumario.getAverage());
+		System.out.println();
+		System.out.println("Folha salarial: " + sumario.getSum());
+
 	}
 }
